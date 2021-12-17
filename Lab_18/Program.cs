8238205ede678e4ff7b8a237babd72265a0cde0d
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-12-17 21:51:26 +0700

    Добавьте файлы проекта.

---
+++ Lab_18/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_18
+{
+    class Program
+    {
+        static bool BracketCheck(string s)
+        {
+            string t = "[{(]})";
+            Stack<char> st = new Stack<char>();
+
+            foreach (var x in s)
+            {
+                int f = t.IndexOf(x);
+
+                if (f >= 0 && f <= 2)
+                    st.Push(x);
+
+                if (f > 2)
+                {
+                    if (st.Count == 0 || st.Pop() != t[f - 3])
+                        return false;
+                }
+            }
+
+            if (st.Count != 0)
+                return false;
+
+            return true;
+        }
+
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(BracketCheck(s) ? "верно" : "не верно");
+            Console.ReadKey();
+        }
+    }
+}
